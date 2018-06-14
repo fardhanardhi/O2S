20b3217a9ebb6023aff718a38fb77334fdb74540
Fardhan Ardhi <fa.dsg.id@gmail.com>
2018-06-15 00:00:00 +0700

    add all activity

---
+++ app/src/main/java/com/solodroid/ecommerce/Config.java
@@ -0,0 +1,17 @@
+package com.solodroid.ecommerce;
+
+import android.annotation.SuppressLint;
+
+public class Config {
+
+    // API URL configuration
+    public static String ADMIN_PANEL_URL = "https://cumaprokerajasih.000webhostapp.com/";
+
+    // change this access similar with accesskey in admin panel for security reason
+    public static String AccessKey = "12345";
+
+    // database path configuration
+    @SuppressLint("SdCardPath")
+    public static String DBPath = "/data/data/com.solodroid.ecommerce/databases/";
+
+}
